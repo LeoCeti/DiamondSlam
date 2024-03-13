e288302727ea42d7a307f33314f4cf6a1be23b4a
LeoCeti <119097453+LeoCeti@users.noreply.github.com>
2024-03-13 10:40:38 -0600

    Actualizacion database.php
    Correcion de nombres de variables.

--- Formulario/database.php
+++ Formulario/database.php
@@ -4,7 +4,7 @@
 $Username= $_POST['Username'];
 $Correo= $_POST['Mail'];
 $Password= $_POST['Password']
-$sql= mysqli_query ($com,"INSERT INTO usuario(id, nombre, correo) values(0,'$Username','$Mail', 'Password')");
+$sql= mysqli_query ($com,"INSERT INTO Usuarios(IDUsuario, Username, Mail, Password) values(0,'$Username','$Mail', '$Password')");
 
 if($sql){
     echo "Usuario Registrado";
